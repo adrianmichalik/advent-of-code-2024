ba1dfefa9d8eae94f62f1a662c7945cd01bb4235
Adrian Michalik <adrian.michalik@connect.is>
2024-12-03 08:01:31 +0100

    Solved part 2, day 3

--- day3/day3.py
+++ day3/day3.py
@@ -29,30 +29,34 @@ def multiply(instruction: str) -> int:
 
 
 def part_two():
-    doable_instructions = []
-    text = ""
+    full_text = ""
     with open('input.txt', 'r') as file:
         for line in file:
-            text += line
-    text = text.replace("\n", "").replace("\r", "")
-    print(text)
-    doable_instructions.extend(find_all_doable_instructions(text))
-    print(doable_instructions)
+            full_text += line
+    sanitized_text = sanitize_text(full_text)
+    text_with_mul_instructions = get_rid_of_not_doable_instructions(sanitized_text)
+    all_mul_instructions = find_all_mul_instructions(text_with_mul_instructions)
     result = 0
-    for instruction in doable_instructions:
+    for instruction in all_mul_instructions:
         result += multiply(instruction)
     print(f"doable instructions result is {result}")
 
 
-def find_all_doable_instructions(text: str) -> list[str]:
-    matches = []
-    pattern = r"do\([^)]*\)((?:(?!don't\().)*?mul\(\d{1,3},\d{1,3}\))+"
-    tmp_matches = re.finditer(pattern, text)
-    for match in tmp_matches:
-        group = match.group(0)
-        matches.extend(re.findall(r"mul\(\d{1,3},\d{1,3}\)", group))
-    print(matches)
-    return matches
+def sanitize_text(text) -> str:
+    pattern = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
+    matches = re.findall(pattern, text)
+    cleaned_text = ''.join(matches)
+    return cleaned_text
+
+
+def get_rid_of_not_doable_instructions(text) -> str:
+    pattern = r"don't\(\).*?do\(\)"
+    cleaned_text = re.sub(pattern, '', text)
+    pattern2 = r"don't\(\).*?$"
+    cleaned_text = re.sub(pattern2, '', cleaned_text, flags=re.MULTILINE)
+    pattern3 = r"do\(\)"
+    cleaned_text = re.sub(pattern3, '', cleaned_text)
+    return cleaned_text
 
 
 if __name__=="__main__":
