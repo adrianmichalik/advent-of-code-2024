5144404462534cded94223150fd0d8980d683f0b
Adrian Michalik <adrian.michalik@connect.is>
2024-12-04 08:35:51 +0100

    day 4 part 2 done

--- day4/day4.py
+++ day4/day4.py
@@ -52,7 +52,7 @@ def search_word(grid, word):
     m = len(grid)
     n = len(grid[0])
 
-    total_count = 0  # Całkowita liczba wystąpień słowa
+    total_count = 0
 
     for i in range(m):
         for j in range(n):
@@ -62,7 +62,36 @@ def search_word(grid, word):
 
 
 def part_two():
-    return None
+    grid = load_grid()
+    result = search_mas(grid)
+    print(f"number of found X-MAS is {result}")
+
+
+def search_mas(grid):
+    m = len(grid)
+    n = len(grid[0])
+    pattern_count = 0
+
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            if grid[i][j] == 'A':
+                corners = [
+                    grid[i - 1][j - 1], # top left
+                    grid[i + 1][j - 1],
+                    grid[i - 1][j + 1],
+                    grid[i + 1][j + 1] # bottom right
+                ]
+                if corners_valid(corners):
+                    pattern_count += 1
+    return pattern_count
+
+
+def corners_valid(corners):
+    return (
+        corners.count('M') == 2 and
+        corners.count('S') == 2 and
+        corners[0] != corners[3]
+    )
 
 
 if __name__=="__main__":
